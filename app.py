0763351cfd1bf240a49bcc76f263dcdbe3922aae
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2018-11-07 10:56:35 -0500

    Added button class Restructure game structure

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from src.Game import Game
+import pygame
 
-game = Game()
-game.main()
+if __name__ == '__main__':
+    # Initializes the pygame screen
+    pygame.init()
+    pygame.display.set_caption("Save the forest")
+    screen = pygame.display.set_mode((1280,736))
+
+    # Initializes the Game
+    game = Game(screen)
+    game.main()
--- src/Game.py
+++ src/Game.py
@@ -1,33 +1,30 @@
 import pygame
+from src.classes.common.Button import Button
 
 class Game:
-    def __init__(self):
+    def __init__(self, screen):
         # Initial Game Properties
         self.level = 0
         self.pause = False
         self.playing = True
-
-        # Initialize Game Window
-        self.window_init("Save the forest", (1280,736))
+        self.screen = screen
 
     def main(self):
         self.create()
         self.update()
 
     def create(self):
+        # Add And Load Background into the game
         self.background = pygame.image.load("./src/assets/images/backyard.png")
+        self.screen.blit(self.background, [0,0])
 
     def update(self):
-        # LOADS THE BACKGROUND INTO THE GAME
-        self.screen.blit(self.background, [0,0])
-        pygame.display.flip()
-
         while self.playing:
-            # Gets all events in Game
-            for event in pygame.event.get():
-                # Closes the Game on exit
-                if event.type == pygame.QUIT:
-                    self.playing = False
+            # All Game Runing Game Events
+            self.all_event()
+
+            # Update the graphics in the game
+            pygame.display.update()
 
     def game_over(self):
         pass
@@ -39,7 +36,8 @@ class Game:
         self.level += 1
         pass
 
-    def window_init(self, title, size):
-        pygame.init()
-        pygame.display.set_caption(title)
-        self.screen = pygame.display.set_mode(size)
+    def all_event(self):
+        for event in pygame.event.get():
+            # Closes the Game on exit
+            if event.type == pygame.QUIT:
+                self.playing = False
