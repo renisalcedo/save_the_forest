3ad59caf42b4d507d0c909b1d8c695f042f1a154
genone <audreytwashington@gmail.com>
2018-11-08 09:59:45 -0500

    in the process of creating character class

---
+++ character.py
@@ -0,0 +1,26 @@
+class Character:
+    def __init__(self, status, level, name): 
+        self.status = {'hp':hp, 'power':power, 'speed':speed, 'cost':cost, defense}
+        self.level = level
+        self.name = name
+  # properties and  method                      
+    def get_status(self, status_key):
+        return self.status[status_key]
+    
+    def recieve_damage(self, dmg)
+        self.status['hp'] -= dmg
+                       
+                       
+                       
+        alive = true
+                       
+                       
+                       
+        
+        if self.status['hp'] <= 0:
+        return              alive = false 
+      
+        
+        if self.status ['hp'] => 0:
+        return
+            
