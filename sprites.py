9ec39965a13ee7dd27e2cd06bf536865a3f22795
Francisco <fjgutierrez112@gmail.com>
2018-11-09 11:26:05 -0500

    created Sprite class

--- sprites.py
+++ sprites.py
@@ -1,11 +1,29 @@
 import pygame
 import sys
 
-def load_image(name):
-    image = pygame.image.load(name)
-    #load new image from a file
-    return image
-
-class TestSprite(pygame.sprite.Sprite):
+class Sprite(pygame.sprite.Sprite):
     # pygame.sprite.Sprite =  Simple base class for visible game objects
     def __init__(self):
+        #pygame.sprite.Sprite.__init__(self): rewriting it as following:
+        super(Sprite, self).__init__()
+        self.img_set = []
+        self.index = 0
+        self.image = self.img_set[index]
+
+    # adding one image to an array of images
+    def add(self, img):
+        img_set.append(img)
+
+    # iterating through set of images
+    def update(self):
+        self.index += 1
+        if self.index == len(self.img_set):
+            self.index = 0
+        
+        self.image = self.img_set[self.index] 
+
+    def animate(self, screen):
+        self.update()
+        self.draw(screen)
+
+    
